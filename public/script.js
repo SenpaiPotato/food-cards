099779409c8f7794fa05ad6d90003575622557f5
SenpaiPotato <minrulee8@gmail.com>
2021-07-23 21:00:08 +0000

    added database and card creation

---
+++ public/script.js
@@ -0,0 +1,77 @@
+//Take inputs and add to database
+const titleForm = document.querySelector("#title-form");
+const tagForm = document.querySelector("#tag-form");
+const foodRef = firebase.database().ref();
+foodRef.on('value', (snapshot) => {
+    const data = snapshot.val();
+    Object.values(data).forEach(card => {
+        const parent = document.querySelector("#grid");
+        const container = document.createElement("div");
+        container.classList.add("card");
+        container.innerHTML = createInnerCard(card);
+        parent.appendChild(container);
+    });
+    
+});
+
+function createInnerCard(card) {
+    return `
+        <!-- CARD -->
+        
+          <div class="card-image">
+            <figure class="image is-square">
+              <img
+                src="${card.imgURL}"
+                alt="${card.title}"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="content">
+              <p class="title is-4">${card.title}</p>
+              <p class="subtitle is-6">${card.tags.map(tag => {
+        return `<span class="tag is-dark">${tag}</span>`;
+    }).join('')}</p>
+            </div>
+          </div>
+
+  `;
+}
+function createCard() {
+    let title = titleForm.value;
+    // TODO Check for URL with re
+    console.log(tagForm.value);
+    let tags = tagForm.value.split(",");
+
+    tags = tags.map(tag => tag.trim());
+
+    titleForm.value = "";
+    tagForm.value = "";
+
+    if (title === "") {
+        alert("Need to input title");
+        return;
+    }
+
+
+    fetch("https://foodish-api.herokuapp.com/api")
+        .then(response => response.json())
+        .then(data => {
+            let card = {
+                imgURL: data.image,
+                title: title,
+                tags: tags
+            };
+            firebase.database().ref().push(card);
+
+            const parent = document.querySelector("#grid");
+            const container = document.createElement("div");
+            container.classList.add("card");
+            container.innerHTML = createInnerCard(card);
+            parent.appendChild(container);
+
+        });
+
+
+
+}
